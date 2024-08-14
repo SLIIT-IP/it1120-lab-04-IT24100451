2aa648793cca33f9e1279d33eb5b2b116ed1490e
IT24100451 <it24100451@my.sliit.lk>
2024-08-14 09:42:13 +0530

    IT24100451 - Mudaligama K.H.C

---
+++ Q3/IT24100451Lab4Q3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class IT24100451Lab4Q3
+{
+   public static void main(String[] args)
+   {
+      Scanner input= new Scanner(System.in);
+      int number;
+   
+      System.out.print("Enter a number:");
+      number=input.nextInt();
+
+      String result=(number>0)?"The number is: Positive"
+                    :(number<0)?"The number is: Negative"
+  	            :"The number is Zero";
+
+      System.out.println(result);
+    }
+}
